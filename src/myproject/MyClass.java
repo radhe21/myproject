1c46f1868ef0d7bd5b4ae30833fd588823d7a83d
radhe21 <mukulchaudhary73@gmail.com>
2022-01-12 15:05:02 +0530

    first commit

---
+++ src/myproject/MyClass.java
@@ -0,0 +1,13 @@
+package myproject;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My Name is Suryansh Chaudhary");
+		System.out.println("I am from Dehradunand I have completed my B.tech From Graphic era hill university Dehradun");
+		System.out.println("I've joined Edubridge Course for enhancing my skills so I could get a desired job as per my qualification");
+	}
+
+}
